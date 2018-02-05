f5c48aa1b583c38f36b749a2b868f3012a2111dd
DineshParthi <dinesh.parthiban92@gmail.com>
2018-02-05 03:06:54 -0500

    ReadMe changes
    Wrote the readMe file with minor changes in other parts

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # -List-User-Repositories-Github
-A program to return the list of public repositories accessible to your user account
+A program to return the list of public repositories accessible to your user account.
+Technologies used: HTML, CSS & Javascript
+
+Instructions to be followed:
+
+1. Download the repository(https://github.com/DineshParthi/-List-User-Repositories-Github.git) to your local machine from GIT.
+2. Open the index.html file in your browser.
+3. Enter any valid username in the text box (on the right side corner) and click submit.
+    a) If it is any invalid git username then pop-up will occur and your page will reload.
+    b) If there are no public public repository associated with the user again a pop-up message will occur.
+    c) If it is a valid git user with more then one record then a new textarea will be displayed with the repositories.
+Note: The result text area will be automatically refreshed each time a user submits a request.
--- js/Fetch_Repositories.js
+++ js/Fetch_Repositories.js
@@ -4,13 +4,12 @@ function fetch_Repo(){
   var url = "https://api.github.com/users/"+gitUserName+"/repos";
   xhttp.open("GET", url , false);
   xhttp.send();
-
   if(xhttp.status == 200)
   {
     var response = JSON.parse(xhttp.response);
     if(response.length == 0)
     {
-      alert("Sorry..!!! There are no repositories for this user!");
+      alert("Sorry..!!! No public repositories for this user!");
       window.location.reload();
     }
     else {
@@ -18,15 +17,13 @@ function fetch_Repo(){
       for (var i=0; i < response.length; i++){
          result = result + response[i].name+"\n";
       }
-      //alert("The user repositories are: "+result.substring(0, result.length-1));
       var resultElement=document.getElementById("result");
       resultElement.innerHTML=result.substring(0, result.length-1);
       resultElement.style.display = "block";
     }
   }
   else {
-   alert("Ooops...!! The git user name is not found!");
+   alert("Ooops...!!! Git user name is not found!");
    window.location.reload();
   }
-
 }
